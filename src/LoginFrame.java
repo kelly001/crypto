ac2417578f5d6c1ae53e25cf213e7ba22712bc09
Julia Zhuravleva <kelly_001@mail.ru>
2014-10-17 17:26:57 +0900

    frames

--- src/LoginFrame.java
+++ src/LoginFrame.java
@@ -3,122 +3,58 @@ import java.awt.event.*;
 import javax.swing.*;
 import javax.swing.border.*;
 
-public class LoginFrame extends JFrame implements ActionListener {
+import java.awt.BorderLayout;
+import java.awt.Color;
+import java.awt.Dimension;
+import java.awt.FlowLayout;
+import java.awt.Font;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
-    private JTextField tfUsername;
-    private JPasswordField pfPassword;
-    private JLabel lbUsername;
-    private JLabel lbPassword;
-    private JButton btnLogin;
-    private JButton btnCancel;
-    private boolean succeeded;
+import javax.swing.JFrame;
 
-    public LoginFrame() {
-        JPanel panel = new JPanel(new GridBagLayout());
-        GridBagConstraints cs = new GridBagConstraints();
-
-        cs.fill = GridBagConstraints.HORIZONTAL;
-
-        lbUsername = new JLabel("Username: ");
-        cs.gridx = 0;
-        cs.gridy = 0;
-        cs.gridwidth = 1;
-        panel.add(lbUsername, cs);
-
-        tfUsername = new JTextField(20);
-        cs.gridx = 1;
-        cs.gridy = 0;
-        cs.gridwidth = 2;
-        panel.add(tfUsername, cs);
-
-        lbPassword = new JLabel("Password: ");
-        cs.gridx = 0;
-        cs.gridy = 1;
-        cs.gridwidth = 1;
-        panel.add(lbPassword, cs);
-
-        pfPassword = new JPasswordField(20);
-        cs.gridx = 1;
-        cs.gridy = 1;
-        cs.gridwidth = 2;
-        panel.add(pfPassword, cs);
-        panel.setBorder(new LineBorder(Color.GRAY));
-
-        btnLogin = new JButton("Login");
-
-        btnLogin.addActionListener(this);
-        btnCancel = new JButton("Cancel");
-        btnCancel.addActionListener(new ActionListener() {
-
-            public void actionPerformed(ActionEvent e) {
-                dispose();
-            }
-        });
-        JPanel bp = new JPanel();
-        bp.add(btnLogin);
-        bp.add(btnCancel);
-
-        getContentPane().add(panel, BorderLayout.CENTER);
-        getContentPane().add(bp, BorderLayout.PAGE_END);
-
-        pack();
-        setResizable(false);
-    }
-
-    public void actionPerformed(ActionEvent ae)
-    {
-
-        if (Login.authenticate(getUsername(), getPassword())) {
-            JOptionPane.showMessageDialog(this,
-                    "Hi " + getUsername() + "! You have successfully logged in.",
-                    "Login",
-                    JOptionPane.INFORMATION_MESSAGE);
-            succeeded = true;
-        } else {
-            JOptionPane.showMessageDialog(LoginFrame.this,
-                    "Invalid username or password",
-                    "Login",
-                    JOptionPane.ERROR_MESSAGE);
-            // reset username and password
-            tfUsername.setText("");
-            pfPassword.setText("");
-            succeeded = false;
-
-        }
-    }
-
-    public String getUsername() {
-        return tfUsername.getText().trim();
-    }
-
-    public String getPassword() {
-        return new String(pfPassword.getPassword());
-    }
-
-    public boolean isSucceeded() {
-        return succeeded;
-    }
-
+public class LoginFrame extends JFrame{
     public static void main(String[] args) {
-        final JFrame frame = new LoginFrame();
+        final JFrame frame = new JFrame("Login Window");
+        frame.setDefaultCloseOperation(JFrame.HIDE_ON_CLOSE);
+        final JLabel infolabel = new JLabel("Hello");
 
-        /*btnLogin.addActionListener(
-                new ActionListener(){
-                    public void actionPerformed(ActionEvent e) {
-                        LoginDialog loginDlg = new LoginDialog(frame);
-                        loginDlg.setVisible(true);
-                        // if logon successfully
-                        if(loginDlg.isSucceeded()){
-                            btnLogin.setText("Hi " + loginDlg.getUsername() + "!");
-                        }
-                    }
-                });*/
 
-        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        final LoginDialog loginDlg = new LoginDialog(frame);
+        loginDlg.setVisible(true);
+
+        // if logon successfully
+        System.out.println(loginDlg.isSucceeded());
+        if(loginDlg.isSucceeded()){
+            infolabel.setText("Hi " + loginDlg.getUsername() + "!\n\r");
+            frame.setVisible(false);
+            frame.dispose();
+
+            MainFrame new_frame = new MainFrame();
+            new_frame.pack();
+            new_frame.setVisible(true);
+        }
+
+        /*loginDlg.addWindowListener(new WindowAdapter() {
+            @Override
+            public void windowClosed(WindowEvent e) {
+                infolabel.setText("action");
+                if(loginDlg.isSucceeded()){
+                    infolabel.setText("Hi " + loginDlg.getUsername() + "!\n\r");
+                    //frame.setVisible(false);
+                    //frame.dispose();
+
+                    /*MainFrame new_frame = new MainFrame();
+                    new_frame.pack();
+                    new_frame.setVisible(true);
+                }
+            }
+        });*/
         frame.setSize(350, 150);
         frame.setLayout(new FlowLayout());
-        //frame.getContentPane().add(btnLogin);
+        frame.getContentPane().add(infolabel);
         frame.setVisible(true);
+
     }
 
 }
--- src/MainFrame.java
+++ src/MainFrame.java
@@ -1,7 +1,37 @@
 import javax.swing.*;
+import java.awt.*;
 
 /**
  * Created by new_name on 14.10.2014.
  */
 public class MainFrame extends JFrame {
+
+    public void createGUI() {
+        setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+        JPanel panel = new JPanel();
+        panel.setSize(350, 150);
+        panel.setLayout(new FlowLayout());
+
+        final JLabel infolabel = new JLabel("Информация");
+        panel.add(infolabel);
+
+        getContentPane().add(panel);
+    }
+
+    public MainFrame() {
+        super("Crypto App");
+        createGUI();
+    }
+
+    public static void main(String[] args) {
+
+        javax.swing.SwingUtilities.invokeLater(new Runnable() {
+            public void run() {
+                JFrame.setDefaultLookAndFeelDecorated(true);
+                MainFrame mainFrame = new MainFrame();
+                mainFrame.pack();
+                mainFrame.setVisible(true);
+            }
+        });
+    }
 }
