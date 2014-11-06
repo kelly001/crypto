bebeb883424623c59141d4d0adc8b73679f23f77
kelly001 <kelly_001@mail.ru>
2014-11-07 02:04:59 +0900

    gui

--- src/CertificatePanel.java
+++ src/CertificatePanel.java
@@ -9,10 +9,19 @@ import java.awt.event.ActionListener;
 import java.awt.event.WindowAdapter;
 import java.awt.event.WindowEvent;
 import java.sql.Time;
+import java.util.logging.Logger;
 
 import com.teacode.swing.dialog
         .OkCancelDialog;
 import com.teacode.swing.component.FieldPanel;
 
 public class CertificatePanel extends FieldPanel {
+
+    protected Logger logger = Logger.getLogger();
+    protected SystemManager manager;
+    protected List<PropertyControl> controls = new ArrayList<PropertyControl>();
+
+   public CertificatePanel () {
+
+    }
 }
