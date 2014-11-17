e4b116cb46488b0b49db70514af2e4fdee741b29
Julia Zhuravleva <kelly_001@mail.ru>
2014-11-17 17:56:47 +0900

    Key update

--- src/database/Key.java
+++ src/database/Key.java
@@ -1,6 +1,7 @@
 package database;
 
-import java.security.Timestamp;
+import java.sql.Timestamp;
+import java.util.Calendar;
 
 /**
  * Created by new_name on 11.11.2014.
@@ -11,4 +12,34 @@ public class Key {
     private Integer type;
     private Timestamp timestamp;
     private Boolean status;
+
+    public Key() {
+        this.status = true;
+        java.util.Date now = Calendar.getInstance().getTime();
+        this.timestamp = new java.sql.Timestamp(now.getTime());
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+    public void setStatus(Boolean status) {
+        this.status = status;
+    }
+    public void setTimestamp(Timestamp time) {
+        this.timestamp = time;
+    }
+    public void setFilename(String filename) {this.filename = filename;}
+    public void setType(Integer type) {this.type = type;}
+
+    public Long getId() {
+        return id;
+    }
+    public Timestamp getTimestamp() {
+        return timestamp;
+    }
+    public Boolean getStatus() {
+        return status;
+    }
+    public String getFilename() {return filename;}
+    public Integer getType() {return type;}
 }
